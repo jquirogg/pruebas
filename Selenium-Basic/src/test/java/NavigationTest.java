82c402a488c44a3107a00fd1946566e19c335c76
jquirogg <juan.quiroga@saviasaludeps.com>
2024-08-06 12:06:21 -0500

    Add files via upload

--- Selenium-Basic/src/test/java/NavigationTest.java
+++ Selenium-Basic/src/test/java/NavigationTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class NavigationTest {
     @Test
     public void navigateToGoogle() throws InterruptedException {
@@ -32,4 +34,37 @@ public class NavigationTest {
 
         }
     }
+
+    @Test
+    public void navigatetoColombia(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jequiroga\\IdeaProjects\\Selenium-Basic\\src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.mercadolibre.com/");
+        try {
+        driver.findElement(By.xpath("//nav/ul/li/a[contains(., 'Colombia')]")).click();
+        String currentUrl = driver.getCurrentUrl();
+
+        assertTrue(currentUrl.contains("www.mercadolibre.com.co"));
+
+        driver.quit();
+        }finally {
+            // Cierra el navegador
+
+        }
+        }
+
+    @Test
+    public void workWithSelects() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jequiroga\\IdeaProjects\\Selenium-Basic\\src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://10.250.3.66:8080/savia/login.faces");
+        driver.findElement(By.cssSelector("input#login\\:usuario")).sendKeys("jquirogg");
+        driver.findElement(By.cssSelector("input#login\\:contrasena")).sendKeys("e0EwIqMH");
+        driver.findElement(By.cssSelector("button[name='login:j_idt23']")).click();
+        Thread.sleep(2000);
+        driver.get("http://10.250.3.66:8080/savia/administracion/alertas.faces");
+        driver.findElement(By.cssSelector("button[name='frmAlertas:j_idt46']")).click();
+        By.cssSelector("input#frmCrear:nombreCrear").findElement(driver).sendKeys("jquirogg");
+    }
+
 }
